b76b3c65f409f29dc2108128b6a72f7475bb5f68
Claire Sersun <claire.sersun@gmail.com>
2022-07-02 13:51:24 -0400

    on load, add a start btn

--- index.js
+++ index.js
@@ -55,7 +55,13 @@ var questionsArr = [
   ]
 
 //on load add a start quiz button
+var quiz = document.querySelector('#quiz')
+var createStartBtn = document.createElement('button')
 //The button MUST have an id attribute of "start-quiz"
+createStartBtn.setAttribute('id', 'start-quiz')
+var startQuizBtnTxt = document.createTextNode('Start Quiz!')
+createStartBtn.appendChild(startQuizBtnTxt)
+quiz.appendChild(createStartBtn)
 
 //If the user has taken the quiz before, the app should ALSO display the previous score
 
@@ -64,6 +70,18 @@ var questionsArr = [
 // in another div show the possible choices as BUTTONS
 //The quiz should also display a timer that counts down from 30 one second at a time
 //use JavaScript's setInterval and clearInterval methods to create the timer.
+// var intervalId = setInterval(function() {
+//   //count down every second from p on page
+//   var seconds = Number(p.textContent) - 1
+//   if (seconds === -1) {
+//     //clear the count down when it gets past 0
+//     clearInterval(intervalId)
+//   } else {
+//     //show the count down
+//     p.textContent = seconds
+//   }
+//   //miliseconds - 1 second == 1000 miliseconds
+// }, 1000)
 
 //Selecting one of the options or running out of time should cause the app to immediately cycle to the next question and set of choices in questionsArr. 
 //There should be no messaging or feedback displayed to the user after making a selection or running out of time
@@ -71,5 +89,12 @@ var questionsArr = [
 //After the last question is answered or time runs out, the game should display the "start quiz" button along with a score 
 
 //score is calculated from the amount of correctly answered questions divided by the total number of questions (figure 2). This number should be rounded to the nearest whole number.
+//var score = 
 
 // JavaScript localStorage API to store the user's most recent score under the key previous-score after each game and retrieve the score on page load
+//localStorage.setItem('nameinstorage',varincode)
+// if (has previous score) {
+//  show previous score
+// } else {
+//   don't show
+// }
